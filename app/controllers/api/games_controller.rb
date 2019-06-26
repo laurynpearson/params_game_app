70a17fc380ee990b6c776bd963e9fcd9a8611f92
Lauryn <laurynpearson@gmail.com>
2019-06-26 18:38:16 -0500

    inital route

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,2 +1,5 @@
 class Api::GamesController < ApplicationController
+  def fun_game
+    render 'game.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
+  namespace :api do
+    get "/param_games" => "games#fun_game"
   #   get "/photos" => "photos#index"
-  # end
+  end
 end
